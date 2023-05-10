727970fcb3e68771355fe252e733a2fc31bfc9ab
IT21188718 <IT21188718@my.sliit.lk>
2023-05-10 12:01:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/financemanagementclient/UserData.kt
@@ -0,0 +1,15 @@
+package com.example.financemanagementclient
+
+data class UserData(val name: String? = null, val operator: String? = null,
+                    val location: String? = null, val phone: String? = null){
+
+}
+data class ExpenseData(val id: String? = null, val type: String? = null,
+                        val amount: String? = null, val date: String? = null){
+
+}
+
+data class IncomeData(val id: String? = null, val type: String? = null,
+                       val amount: String? = null, val date: String? = null){
+
+}
